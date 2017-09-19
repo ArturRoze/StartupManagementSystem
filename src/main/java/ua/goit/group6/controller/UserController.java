9e83ed34ead91a195bbc385f275d67db61d9b622
Vancho <ivpavboyko@gmail.com>
2017-09-19 18:04:28 +0300

    user profile delete and update_form

--- src/main/java/ua/goit/group6/controller/UserController.java
+++ src/main/java/ua/goit/group6/controller/UserController.java
@@ -3,10 +3,15 @@ package ua.goit.group6.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import ua.goit.group6.model.User;
 import ua.goit.group6.service.UserService;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/users")
@@ -23,5 +28,33 @@ public class UserController {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
     }
+
+    @GetMapping("/profile/{id}")
+    public ModelAndView profile(@PathVariable("id") String idString) {
+        ModelAndView profile = new ModelAndView("profile");
+        long id = Long.parseLong(idString);
+        User user = userService.getById(id);
+        profile.addObject("user", user);
+        return profile;
+    }
+
+    @GetMapping("/profile/{id}/delete")
+    public String delete(@PathVariable("id") String idString) {
+        long id = Long.parseLong(idString);
+        User user = userService.getById(id);
+        userService.delete(user);
+        //TODO make deleteById(id)
+        return "redirect:/";
+    }
+
+    @GetMapping("/profile/{id}/update")
+    public ModelAndView update(@PathVariable("id") String idString) {
+        ModelAndView updateForm = new ModelAndView("update_form");
+        long id = Long.parseLong(idString);
+        User user = userService.getById(id);
+        updateForm.addObject("user", user);
+        return updateForm;
+    }
+
 
 }
